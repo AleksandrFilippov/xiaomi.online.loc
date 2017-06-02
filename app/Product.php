afaedcdba76d737d4e54ecf185b6d86ceabc64de
Aleksandr <help_to_you@mail.ru>
2017-06-02 15:40:40 +0300

    удалили ненужную модель

--- app/Product.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App;
-use Illuminate\Database\Eloquent\Model;
-
-class Product extends Model
-{
-	protected $table = 'portfolios';
-    protected $fillable = ['id','name','filter','images','price'];
-}
