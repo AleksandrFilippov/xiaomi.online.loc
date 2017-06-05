bf1276df98b405246738b909a470ab41745cdc88
Denis Boltikov <pokerdn2010@gmail.com>
2017-06-06 00:06:45 +0700

    Рефакторинг вьюхи страницы списка страниц

--- resources/views/admin/content_pages.blade.php
+++ resources/views/admin/content_pages.blade.php
@@ -1,6 +1,6 @@
-<div style="margin:0px 50px 0px 50px;">   
+<div style="margin:0px 50px 0px 50px;">
 
-@if($pages)
+    @if($pages->isNotEmpty())
  
 	<table class="table table-hover table-striped">
         <thead>
@@ -8,7 +8,6 @@
                 <th>№ п/п</th>
                 <th>Имя</th>
                 <th>Псевдоним</th>
-                <th>Текст</th>
                 <th>Дата создания</th>
                 
                 <th>Удалить</th>
@@ -23,7 +22,6 @@
         		<td>{{ $page->id }}</td>
         		<td>{!! Html::link(route('pagesEdit',['page'=>$page->id]),$page->name,['alt'=>$page->name]) !!}</td>
         		<td>{{ $page->alias }}</td>
-        		<td>{{ $page->text }}</td>
         		<td>{{ $page->created_at }}</td>
         		
         		<td>
