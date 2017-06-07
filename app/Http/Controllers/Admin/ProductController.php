991ab9b601fec4c0129e08692c5ea54dd3e8078b
Aleksandr <help_to_you@mail.ru>
2017-06-07 19:33:40 +0300

    Кодинг контроллера вида вывода списка товара; Кодинг вьюхи вывода списка товара.

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdatePageRequest;
+use App\Http\Requests\StorePageRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Http\Request;
+use App\Product;
+
 
 class ProductController extends Controller
 {
@@ -16,6 +22,44 @@ class ProductController extends Controller
     {
         $products = Product::all();
 
-        return view('admin.product.index', ['products' => $products]);
+        $data = [
+            'title' => 'Продукция',
+            'products' => $products
+        ];
+
+        return view('admin.product.index', $data);
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(StoreProductRequest $request)
+    {
+
+    }
+
+    public function edit(Request $request, Page $product)
+    {
+
+    }
+
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+
+    }
+
+    /**
+     * Удаление товара из списка товаров
+     *
+     * @param Product $product
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function delete(Product $product)
+    {
+        $product->delete();
+
+        return redirect()->route('admin.products.index')->with('status', 'Товар удален');
     }
 }
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,7 +1,45 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Sasha
- * Date: 07.06.2017
- * Time: 17:50
- */
+@extends('layouts.admin')
+
+@section('header')
+    @include('admin.header')
+@endsection
+
+@section('content')
+    <div style="margin:0px 50px 0px 50px;">
+        {!! Html::link(route('admin.products.create'),'Добавить товар') !!}
+        @if($products)
+            <table class="table table-hover table-striped">
+                <thead>
+                <tr>
+                    <th>№ п/п</th>
+                    <th>Имя</th>
+                    <th>Фильтр</th>
+                    <th>Изображение</th>
+                    <th>Цена</th>
+                    <th>Дата создания</th>
+                    <th>Удалить</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($products as $k => $product)
+                    <tr>
+                        <td>{{$product->id}}</td>
+                        <td>  {!! Html::link(route('admin.products.edit',['product'=>$product->id]),$product->name,['alt'=>$product->name]) !!}  </td>
+                        <td>{{$product->filter}}</td>
+                        <td>{!! Html::image('assets/img/'.$product->images,'', array('style' => 'width:150px' )) !!}</td>
+                        <td>{{$product->price}}</td>
+                        <td>{{$product->created_at}}</td>
+                        <td>
+                            {!! Form::open(['url' => route('admin.products.edit',['product'=>$product->id]),'class'=>'form-horizontal','method'=>'POST']) !!}
+                            {{ method_field('DELETE') }}
+                            {!! Form::button('Удалить', ['class' => 'btn btn-danger','type'=>'submit']) !!}
+                            {!! Form::close() !!}
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @endif
+        {!! Html::link(route('admin.products.create'),'Добавить товар') !!}
+    </div>
+@endsection
