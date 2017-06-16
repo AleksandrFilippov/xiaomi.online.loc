eaefc1c2ac38af99cb1283fe78bcd2c166940e0e
Aleksandr <help_to_you@mail.ru>
2017-06-17 01:51:21 +0300

    контроллер для масс выгрузки товара из прайса

---
+++ app/Http/Controllers/Admin/LoadProductsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LoadProductsController extends Controller
+{
+    /**
+     * Вывод формы загрузки прайса
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        $data = [
+            'title' => 'Выгрузка товара из прайса',
+        ];
+
+        return view('admin.loadproducts.create', $data);
+    }
+
+    /**
+     *
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('uploadfile');
+    }
+
+    /**
+     * логика загрузки прайса
+     */
+    public
+    function showUploadFile(Request $request)
+    {
+        $file->move($destinationPath, $file->getClientOriginalName());
+    }
+}
