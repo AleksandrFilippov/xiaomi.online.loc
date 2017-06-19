6101e031cd94b092c43787394b73cd794494f96a
Aleksandr <help_to_you@mail.ru>
2017-06-19 17:22:21 +0300

    добавил оригинальное расширение к перемещаемому сайту

--- app/Http/Controllers/Admin/UploadFileController.php
+++ app/Http/Controllers/Admin/UploadFileController.php
@@ -1,25 +1,29 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use uploadfile;
 
-class UploadFileController extends Controller {
-   public function index(){
-       $data = [
-           'title' => 'Загрузка прайса',
-       ];
+class UploadFileController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Загрузка прайса',
+        ];
 
-       return view('admin.uploadfile.index', $data);
-   }
+        return view('admin.uploadfile.index', $data);
+    }
 
     public function uploadfile(Request $request)
     {
-      $file = $request->file('image');
+        $file = $request->file('image');
 
         //Move Uploaded File
-      $destinationPath = 'assets\xls';
-      $file->move($destinationPath,$file->getClientOriginalName());
-   }
+        $destinationPath = 'assets\xls';
+        $file->move($destinationPath, $file->getClientOriginalName(), '.', $file->getClientOriginalExtension());
+    }
 }
