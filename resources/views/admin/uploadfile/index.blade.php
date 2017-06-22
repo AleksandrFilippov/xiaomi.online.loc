f9d8fa257094a3d8b9879c2d61fc80e754b3076d
Denis Boltikov <pokerdn2010@gmail.com>
2017-06-22 14:35:31 +0700

    Правка текста

--- resources/views/admin/uploadfile/index.blade.php
+++ resources/views/admin/uploadfile/index.blade.php
@@ -8,7 +8,7 @@
     <div class="wrapper container-fluid">
         {!! Form::open(['url' => route('uploadfile'),'class'=>'form-horizontal','method'=>'POST']) !!}
         <div class="form-group">
-            {!! Form::label('images', 'Файл должен быть назван и иметь расширение: Price.xls',['class'=>'col-xs-2 control-label']) !!}
+            {!! Form::label('images', 'Файл должен иметь название Price.xls',['class'=>'col-xs-2 control-label']) !!}
             <div class="col-xs-8">
                 {!! Form::file('images', ['class' => 'filestyle','data-buttonText'=>'Выберите прайс','data-buttonName'=>"btn-primary",'data-placeholder'=>"Файла нет"]) !!}
             </div>
